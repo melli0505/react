63ffa677fdf61a6e6b0687a703463fc5418c2790
Kang Dain <dmelli0505@gmail.com>
2021-01-13 20:01:04 +0900

    9. component-styling - 2

---
+++ component-styling/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import SassComponent from'./SassComponent';
+
+function App() {
+  return (
+    <div>
+      <SassComponent></SassComponent>
+    </div>
+  );
+}
+
+export default App;
