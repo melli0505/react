eefcde7a3c1d3a5a5913d337da0e4010dd2c8524
Kang Dain <dmelli0505@gmail.com>
2021-01-21 02:49:45 +0900

    add home&about page

---
+++ spa-route/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                홈
+            </h1>
+            <p>
+                이 페이지는 가장 먼저 보여지는 홈 페이지입니다.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
