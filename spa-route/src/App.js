960b8bd8d64c9c7b4a5eefb88548a03da3a03062
Kang Dain <dmelli0505@gmail.com>
2021-01-21 02:50:03 +0900

    use Route, Link

--- spa-route/src/App.js
+++ spa-route/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      {/* <Route path="/about" component={About} /> */}
+      <Route path={['/about', '/info']} component={About} />
     </div>
   );
-}
+};
 
 export default App;
